a2834467aec25f77e77b53721a972de64855a571
Arturo May <artur.malvarez92@gmail.com>
2019-05-20 17:32:36 -0500

    The end
    All the examples are finished

--- PracticeExercise.py
+++ PracticeExercise.py
@@ -42,7 +42,7 @@ def vocal(x):
         return True
     return False
 
-#  create a function for addition and one for multiplication of all the elements of a LIST
+#  create a function for addition and one for multiplication of all the elements of a LIST 5
 def suma(lista):
     suma = 0
     for i in range (0,(len(lista)),1):
@@ -61,11 +61,67 @@ def mult(lista):
 # print the invers value that was received 6
 def invers(cadena):
     cont = len(cadena)
+    cadena2 = ""
     for i in range (cont-1,-1,-1):
-        print(cadena[i]) 
-        
-    
-    
-    
+        cadena2 += cadena[i]
+    return cadena2
 
-print(max2(10,2))
+# Check if a word is palindrome or no 7
+def palindrome(cadena):
+    
+    cadena2 = invers(cadena)   
+    if cadena == cadena2:
+        return True
+    return False
+
+# Compare 2 list and check if one of the elemnts of 1 list is in the other one 8
+def superposicion (lista1, lista2):
+    for i in lista1:
+        for x in lista2:
+            if i == x:
+                return True
+    return False
+
+'''
+Create a function the get an int n and a char as parameters the return
+the character repeated 9
+'''
+def generated_n_char (n, c):
+    result = ""
+    '''
+    Another way to do this will be only writing this
+    return n * c
+    '''
+    for i in range(n):
+        result += c    
+    return result
+
+'''
+Definir un histograma procedimiento() que tome una lista 
+de números enteros e imprima un histograma en la pantalla. 10
+'''
+def procedimiento(values):
+    for i in values:
+        print( i*"x") 
+        
+# Print the first repeat letter inside a word
+def repeat(word):
+   
+    for i in range(len(word)):
+        print(i)
+      
+        for j in range(i +1, len(word), +1):
+            if(word[i] == word[j]):
+                return word[i]
+            
+    return False
+            
+            
+            
+            
+        
+        
+
+
+
+
